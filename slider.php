29a98cec892b25ef8d8a883d42dff845afd3e6f4
Edwar11100 <edouard.gravez@laposte.net>
2015-11-03 10:32:19 +0100

    Ajout des images modifiées en 400*600px, du slider.php et du slider.css ::::::Veuillez saisir le message de validation pour vos modifications. Les lignes

---
+++ slider.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+  <head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="slider.css" />
+        <!--[if lt IE 9]>
+            <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+	<script src="https://raw.github.com/LeaVerou/prefixfree/gh-pages/prefixfree.min.js"></script> <!-- Fichier JavaScript assure support CSS3   -->
+  </head>
+	<body>
+	  <div id="galerie-menu">
+		<nav>
+      <a href="#" tabindex="0" title="Alexis CHALONO"><img src="Photos/Alexis_CHALONO.JPG"  alt="Alexis CHALONO"  />
+      </a><a href="#" tabindex="0" title="Aline ELWERT"><img src="Photos/Aline_ELWERT.JPG"  alt="Aline ELWERT"  />
+			</a><a href="#" tabindex="0" title="Amandine MICHEL"><img src="Photos/Amandine_MICHEL.JPG"  alt="Amandine MICHEL"  />
+			</a><a href="#" tabindex="0" title="Benjamin BULLICH"><img src="Photos/Benjamin_BULLICH.JPG"  alt="Benjamin_BULLICH"  />
+			</a><a href="#" tabindex="0" title="Catherine STRUGALA"><img src="Photos/Catherine_STRUGALA.JPG"  alt="Catherine STRUGALA"  />
+			</a><a href="#" tabindex="0" title="Edouard GRAVEZ"><img src="Photos/Edouard_GRAVEZ.JPG"  alt="Edouard GRAVEZ"  />
+			</a><a href="#" tabindex="0" title="Gregory GUILLAUME"><img src="Photos/Gregory_GUILLAUME.JPG"  alt="Gregory GUILLAUME"  />
+			</a><a href="#" tabindex="0" title="Gwenaelle GUEZOU"><img src="Photos/Gwenaelle_GUEZOU.JPG"  alt="Gwenaelle GUEZOU"  />
+			</a><a href="#" tabindex="0" title="Isabelle Gomes Da Costa"><img src="Photos/Isabelle_Gomes_Da_Costa.jpg"  alt="Isabelle Gomes Da Costa"  />
+
+			</a><a href="#" tabindex="0" title="Jeremy RAYNAUD"><img src="Photos/Jeremy_raynaud.JPG" alt="Jeremy RAYNAUD" />
+      </a><a href="#" tabindex="0" title="Jerome NANGUET"><img src="Photos/Jerome_NANGUET.JPG" alt="Jerome NANGUET" />
+      </a><a href="#" tabindex="0" title="Julie Michelet"><img src="Photos/Julie_Michelet.JPG" alt="Julie Michelet" />
+      </a><a href="#" tabindex="0" title="Nicolas Devynck"><img src="Photos/Nicolas_Devynck.JPG" alt="Nicolas Devynck" />
+      </a><a href="#" tabindex="0" title="Otman HANAN"><img src="Photos/Otman_Hanan.JPG" alt="Otman HANAN" />
+      </a><a href="#" tabindex="0" title="Patrick BELAIRE"><img src="Photos/Patrick_BELAIRE.JPG" alt="Patrick BELAIRE" />
+      </a><a href="#" tabindex="0" title="Patrick MORRGHADI"><img src="Photos/Patrick_Morghadi.JPG" alt="Patrick MORRGHADI" />
+      </a><a href="#" tabindex="0" title="Philippe Colas"><img src="Photos/Philippe_Colas.jpg" alt="Philippe Colas" />
+      </a><a href="#" tabindex="0" title="Thomas NGOMA"><img src="Photos/Thomas_NGOMA.JPG" alt="Thomas NGOMA" />
+
+      </a><a href="#" tabindex="0" title="Tyffany ZILLIOX"><img src="Photos/Tyffany_ZILLIOX.JPG" alt="Tyffany ZILLIOX" />
+      </a><a href="#" tabindex="0" title="Ulysse GABELLI"><img src="Photos/Ulysse_GABELLI.JPG" alt="Ulysse GABELLI" />
+      </a><a href="#" tabindex="0" title="Renaud VIVANCOS"><img src="Photos/VIVANCOS_Renaud.JPG" alt="Renaud VIVANCOS" />
+      </a><a href="#" tabindex="0" title="David TARCZEWSKI"><img src="Photos/david_TARCZEWSKI.JPG" alt="David TARCZEWSKI" />
+      </a><a href="#" tabindex="0" title="Emmanuel DELOS"><img src="Photos/delos_emmanuel.JPG" alt="Emmanuel DELOS" />
+		</a></nav>
+	  </div>
+	</body>
+</html>
